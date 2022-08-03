30477d70caddbbf8e84ad9ca636a5fe48caaa83f
Salman Shaikh <tony903212@gmail.com>
2022-08-03 19:37:56 +0530

    Added basic remaining time display

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import OrderForm from "components/OrderForm";
 import OrderQueue from "components/OrderQueue";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import styles from "assets/css/pages/Home.module.css";
 import { StateContext } from "state/GlobalState";
 
 function Home() {
-    const [{ orderId }] = useContext(StateContext);
+    const [{ orderId, orderQueue }] = useContext(StateContext);
+    const [remainingTime, setRemainingTime] = useState(0)
+
+    useEffect(() => {
+        const index = orderQueue.findIndex(order => order._id === orderId)
+        setRemainingTime(orderQueue.slice(0, index).reduce((x, y) => x.service.approxTime + y?.service?.approxTime || 0))
+    }, [orderId, orderQueue])
     return (
         <>
             <div className={styles.queueContainer}>
@@ -18,7 +24,10 @@ function Home() {
                     <OrderForm />
                 </div>
             ) : (
-                <h2>Thanks for placing order. your token is: {orderId}</h2>
+                <>
+                    <h2>Thanks for placing order. your token is: {orderId}</h2>
+                    <h2>{Math.floor(remainingTime / 60)} hours {remainingTime % 60} minutes waiting</h2>
+                </>
             )}
         </>
     );
